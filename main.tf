ce51b9db9f873c722f92f0cd22dce93c2203fcfb
nikhil <nikhilnaidu980@gmail.com>
2025-02-13 10:26:56 -0500

    Added Iam role and policy

--- main.tf
+++ main.tf
@@ -42,3 +42,31 @@ resource "aws_security_group_rule" "allow_ingress" {
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.eks.id
 }
+
+# IAM Role for EKS Cluster
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+}
+  
+# Attach policy for EKS Cluster management
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attachment" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+  
+# Attach policy for VPC management (needed by EKS to configure networking)
+resource "aws_iam_role_policy_attachment" "eks_vpc_policy_attachment" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
+}
